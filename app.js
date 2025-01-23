ed69f5d506a5edccc513399008693505c8537e02
Jhonata David <jhonatadavidf@gmail.com>
2025-01-23 16:20:36 -0300

    criado a funçao de campo vazio

--- app.js
+++ app.js
@@ -4,13 +4,18 @@ function adicionarAmigo() {
   let input = document.getElementById('amigo');   // Seleciona o input pelo ID
   let nomesDentroDoInput = input.value.trim();  // Pega o valor digitado no input / o .trim() retira espacos desnecessarios
   listaDeNomes.push(nomesDentroDoInput); //pega a lista de nomes e adiciona os nomes que estao dentro do input / o metodo push adiciona algo a lista do array
-  console.log(listaDeNomes);
+  campoVazio();
 }
 
 function gerarNomesAleatorio() {
     let nomeAleatorio = listaDeNomes[Math.floor(Math.random() * listaDeNomes.length)];
-    console.log(nomeAleatorio);
+    return nomeAleatorio;
 }
 function sortearAmigo(){
-    
+    alert(gerarNomesAleatorio());
+}
+
+function campoVazio(){
+  let input = document.querySelector('input');
+  input.value = '';
 }
