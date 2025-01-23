594f25a93b85a85b4761d0376c69cfda52cb6441
JhonataDavid <jhonatadavidf@gmail.com>
2025-01-23 05:59:04 -0300

    criando a funcao adicionar amigo e a funcao gerar nomes aleatorios

--- app.js
+++ app.js
@@ -1,2 +1,16 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. 
-//Aqui você deverá desenvolver a lógica para resolver o problema.
+let listaDeNomes = [];
+
+function adicionarAmigo() {
+  let input = document.getElementById('amigo');   // Seleciona o input pelo ID
+  let nomesDentroDoInput = input.value.trim();  // Pega o valor digitado no input / o .trim() retira espacos desnecessarios
+  listaDeNomes.push(nomesDentroDoInput); //pega a lista de nomes e adiciona os nomes que estao dentro do input / o metodo push adiciona algo a lista do array
+  console.log(listaDeNomes);
+}
+
+function gerarNomesAleatorio() {
+    let nomeAleatorio = listaDeNomes[Math.floor(Math.random() * listaDeNomes.length)];
+    console.log(nomeAleatorio);
+}
+function sortearAmigo(){
+    
+}
